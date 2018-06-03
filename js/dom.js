60927d2adee73491bc90a7921d06c972dda04874
Johnny Mast <mastjohnny@gmail.com>
2018-06-03 17:59:58 +0200

    Updated the comments for the _dom functions.

--- js/dom.js
+++ js/dom.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * Construct with a selector thats all to it.
+ * Construct with a selector that's all to it.
  *
  * @param selector
  * @private
@@ -22,13 +22,18 @@ _dom.prototype.hide = function () {
 /**
  * Ad a EventListener for the domelement
  *
- * @param {Evennt} event The x evemt
+ * @param {Event event The x evemt
  * @param callback
  */
 _dom.prototype.on = function (event, callback) {
   this.elm.addEventListener(event, callback)
 }
 
+/**
+ * Return the actual dom element.
+ *
+ * @returns {HTMLElement | HTMLSelectElement | HTMLLegendElement | HTMLTableCaptionElement | HTMLTextAreaElement | HTMLModElement | HTMLHRElement | HTMLOutputElement | HTMLPreElement | HTMLEmbedElement | HTMLCanvasElement | HTMLFrameSetElement | HTMLMarqueeElement | HTMLScriptElement | HTMLInputElement | HTMLUnknownElement | HTMLMetaElement | HTMLStyleElement | HTMLObjectElement | HTMLTemplateElement | HTMLBRElement | HTMLAudioElement | HTMLIFrameElement | HTMLMapElement | HTMLTableElement | HTMLAnchorElement | HTMLMenuElement | HTMLPictureElement | HTMLParagraphElement | HTMLTableDataCellElement | HTMLTableSectionElement | HTMLQuoteElement | HTMLTableHeaderCellElement | HTMLProgressElement | HTMLLIElement | HTMLTableRowElement | HTMLFontElement | HTMLSpanElement | HTMLTableColElement | HTMLOptGroupElement | HTMLDataElement | HTMLDListElement | HTMLFieldSetElement | HTMLSourceElement | HTMLBodyElement | HTMLDirectoryElement | HTMLDivElement | HTMLUListElement | HTMLHtmlElement | HTMLAreaElement | HTMLMeterElement | HTMLAppletElement | HTMLFrameElement | HTMLOptionElement | HTMLImageElement | HTMLLinkElement | HTMLHeadingElement | HTMLSlotElement | HTMLVideoElement | HTMLBaseFontElement | HTMLTitleElement | HTMLButtonElement | HTMLHeadElement | HTMLParamElement | HTMLTrackElement | HTMLOListElement | HTMLDataListElement | HTMLLabelElement | HTMLFormElement | HTMLTimeElement | HTMLBaseElement | null | *}
+ */
 _dom.prototype.get = function () {
   return this.elm
 }
@@ -50,6 +55,7 @@ _dom.prototype.prop = function (attribute, value) {
 }
 
 /**
+ * Append a html string to the end if the element.
  *
  * @param html
  */
@@ -58,6 +64,8 @@ _dom.prototype.append = function (html) {
 }
 
 /**
+ * Get the value of element. If value is passed the value will be set
+ * to this value instead.
  *
  * @param value
  * @returns {string | number}
@@ -70,10 +78,11 @@ _dom.prototype.val = function (value) {
 }
 
 /**
+ * Return a new instance of _dom (the jquery clone) so the code could
  *
  * @param selector
  * @returns {_dom}
  */
-let dom = (selector) => {
+let dom = function (selector) {
   return new _dom(selector)
 }
